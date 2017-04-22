a19ad703e8524abc2b5cb08dc5062197942d4672
Maksim Rakitin <maxim.rakitin@gmail.com>
2017-04-22 14:04:07 -0400

    build file for Linux added.

---
+++ build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# General options:
+tmpdir='tmp'
+owner='RakitinMS'
+
+# Files to generate pdfs for:
+cv="${owner}_CV"
+jobs="${owner}_jobs"
+edu="${owner}_edu"
+awards="${owner}_awards"
+skills="${owner}_skills"
+refs="${owner}_refs"
+pubs="${owner}_pubs"
+confs="${owner}_confs"
+
+# Create a temp dir if it does not exist:
+if [ ! -d "$tmpdir" ]; then
+    mkdir $tmpdir
+fi
+
+# LaTeX options:
+outdir="--output-directory=$tmpdir"
+nonstop="-interaction=nonstopmode"
+
+# GhostScript parameters:
+gs="ghostscript"
+compatibility="1.4"
+quality="printer"
+
+for b in $cv $pubs $jobs $edu $awards $skills $refs $pubs $confs; do
+    texfile="${b}.tex"
+    auxfile="${b}.aux"
+    pdffile="${b}.pdf"
+    finalpdf="${b}.pdf"
+    compressedpdf="${b}_comp.pdf"
+
+    # Sequence of LaTeX commands from https://tex.stackexchange.com/a/13012.
+    latex $nonstop $outdir $texfile
+    bibtex ${tmpdir}/${auxfile}
+    latex $nonstop $outdir $texfile
+    pdflatex -synctex=1 $nonstop --src-specials $outdir $texfile
+
+    # Copy the resulted file from the temp dir:
+    cp -fv ${tmpdir}/${pdffile} $finalpdf
+
+    # Compress the file:
+    $gs -sDEVICE=pdfwrite -dCompatibilityLevel=$compatibility -dPDFSETTINGS=/$quality -dNOPAUSE -dQUIET -dBATCH -q -sOutputFile=$compressedpdf $finalpdf
+    mv -fv $compressedpdf $finalpdf
+done
+
+
+exit 0
+
