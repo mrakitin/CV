caaf565f0c61ae90685d0f41aa034623be26810a
Maksim Rakitin <mrakitin@bnl.gov>
2020-04-22 14:23:32 -0400

    CI: fix Docker image to use

--- docker-build.sh
+++ docker-build.sh
@@ -6,5 +6,10 @@ mntdir='/build'
 
 command="apt-get update && apt-get install ghostscript -y && cd ${mntdir} && ./build.sh"
 
-docker pull texlive/texlive-full:2017
-docker run -it --rm -v $PWD:${mntdir} texlive/texlive-full:2017 bash -c "$command"
+# The following Docker image does not exist or is not public as of 04/22/2020:
+# docker pull texlive/texlive-full:2017
+
+DOCKER_IMAGE=mirisbowring/texlive_ctan_full
+
+docker pull ${DOCKER_IMAGE}
+docker run -it --rm -v $PWD:${mntdir} ${DOCKER_IMAGE} bash -c "$command"
