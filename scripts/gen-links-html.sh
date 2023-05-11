9d48367ad1715794365a3943a8b43718d1e10bad
Max Rakitin <mrakitin@bnl.gov>
2023-05-11 02:39:24 -0400

    Add html report on linkcheck and add more sources with http(s) links

--- scripts/gen-links-html.sh
+++ scripts/gen-links-html.sh
@@ -13,8 +13,16 @@ echo "Scripts dir : ${SCRIPTS_DIR}"
 echo "Links file  : ${LINKS_FILE}"
 echo "HTML file   : ${HTML_FILE}"
 
-git grep -r -o -E '{http.*://.*}' ${REPO_ROOT}/**.tex ${REPO_ROOT}/src/**.tex | cut -d'{' -f2 | cut -d'}' -f1 | grep ^http | sed 's/\\//g' | sort -u > ${LINKS_FILE}
+git grep -r -o -E '{http.*://.*}' \
+    ${REPO_ROOT}/**.tex \
+    ${REPO_ROOT}/src/**.tex \
+    ${REPO_ROOT}/**.bib \
+    | cut -d'{' -f2 | cut -d'}' -f1 | grep ^http | sed 's/\\//g' | sort -u > ${LINKS_FILE}
 
 python ${SCRIPTS_DIR}/format-html.py "${LINKS_FILE}" "${HTML_FILE}"
 
-linkchecker --check-extern --config=${REPO_ROOT}/.linkcheckerrc ${HTML_FILE}
+linkchecker \
+    --config=${REPO_ROOT}/.linkcheckerrc \
+    --check-extern \
+    --file-output html/all-links-report.html \
+    ${HTML_FILE}
